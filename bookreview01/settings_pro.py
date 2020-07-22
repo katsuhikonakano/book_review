9f2734c518c21c90f542b02b3d407491738df29d
katsuhiko <katsuhiko1211@gmail.com>
2020-07-22 21:50:36 +0900

    third commit

--- bookreview01/settings_pro.py
+++ bookreview01/settings_pro.py
@@ -3,13 +3,11 @@ from .settings import *
 DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-STATIC_ROOT = 'usr/share/nginx/html/static'
-MEDIA_ROOT ='usr/share/nginx/html/media'
+STATIC_ROOT = '/usr/share/nginx/html/static'
+MEDIA_ROOT ='/usr/share/nginx/html/media'
 
 LOGGING = {
     'version': 1,
